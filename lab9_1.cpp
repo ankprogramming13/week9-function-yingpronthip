1383f1d2317fff30722cba5ba9b2c786ccc2f1f0
Unknown <pronthipza@gmail.com>
2018-01-19 13:39:28 +0700

    ข้อที่ 1

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,13 +2,37 @@
 
 using namespace std;
 
+void swapNumber (int *x, int *y) {
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void Showvalue (int x, int y){
+    cout << "This is x : " << x << endl;
+    cout << "This is y : " << y << endl;
+}
+
 int main() {
     /*เขียนโปรแกรมรับค่าจำนวนเต็มบวกจากคีย์บอร์ด 2 จำนวน เก็บไว้ในตัวแปร และสลับค่าข้อมูลระหว่างตัวแปรสองตัวแปรดังกล่าว
     โดยกำหนดให้โปรแกรมสลับค่าข้อมูลจนกว่าผู้ใช้งานต้องการออกจากการทำงานของโปรแกรม
     และแบ่งการทำงานของโปรแกรมเป็นโปรแกรมย่อย
     */
+    int a,b;
+    cin>>a;
+    cin>>b;
+    Showvalue(a,b);
 
+    int cmd;
 
+    do{
+        cin >> cmd;
+        if (cmd=1){
+            swapNumber(&a,&b);
+            Showvalue(a,b);
+        }
+    }while(cmd != 2);
 
     return 0;
 }
